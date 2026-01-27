cf0d5b4fe93c00c7857d59b12235aa96a760b55a
Youssef Boudouar <youssefboudouar77@gmail.com>
2026-01-27 15:59:29 +0100

    Initial commit: Setup Laravel with Database and Models

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductsFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'image_url', 'stock', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
