b7cfcf1ad798e6f633b45bc100561461547b0865
youssef-boudouar <youssefboudouar771@gmail.com>
2026-02-02 01:22:21 +0100

    Add Category model import in DatabaseSeeder and ensure seeding of categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,5 +22,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            CategorySeeder::class,
+            ProductsSeeder::class,
+        ]);
     }
 }
+
