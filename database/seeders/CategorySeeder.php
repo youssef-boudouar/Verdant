ed38cc421b55735945a0c2e04a7f4c7b1fae1a3f
Youssef Boudouar <youssefboudouar77@gmail.com>
2026-02-02 01:22:12 +0100

    Add CategorySeeder for seeding initial category data

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Plantes',
+            'description' => 'dsbjhbchxnxijcdnokwodkwpe',
+        ]);
+
+        Category::create([
+            'name' => 'Graines',
+            'description' => 'dsiudsffsdfcsdcsdccd'
+        ]);
+
+        Category::create([
+            'name' => 'Outils',
+            'description' => 'dsddddddddcxcxvxcvflpokdvpo',
+        ]);
+    }
+}
