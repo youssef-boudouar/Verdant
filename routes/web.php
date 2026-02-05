8ea5f9964831014e56891fc970d60a9b3dfd7cff
Youssef Boudouar <youssefboudouar77@gmail.com>
2026-02-05 16:54:04 +0100

    Add authentication and favorites routes to enhance user functionality

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +12,13 @@ Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/search', [ProductController::class, 'search'])->name('search');
 Route::resource('products', ProductController::class);
 
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+
+Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
+Route::post('/favorites/{product}', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
