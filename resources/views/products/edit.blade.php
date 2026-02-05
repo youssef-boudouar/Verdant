57e1e9a9edfb584617476890428500b7519346f3
Youssef Boudouar <youssefboudouar77@gmail.com>
2026-02-05 16:53:47 +0100

    Fix formatting of Blade directives in edit product view

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,6 +1,5 @@
-
-<!-- @extends('layouts.app') -->
-<!-- @section('content') -->
+ @extends('layouts.app')
+ @section('content')
 
 <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 lg:py-12">
 
@@ -179,4 +178,4 @@
 
 </div>
 
-<!-- @endsection -->
+ @endsection 
