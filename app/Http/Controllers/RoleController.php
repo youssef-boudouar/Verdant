494b113647b32c6ce881af410c24794723a5cf0f
youssef-boudouar <youssefboudouar771@gmail.com>
2026-02-18 17:18:42 +0000

    Add RoleController for managing roles and assigning permissions

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        return view('roles.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $permissions = Permission::all();
+        return view('roles.create', compact('permissions'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:roles,name',
+            'permissions' => 'array',
+        ]);
+
+        $role = Role::create(['name' => $validated['name']]);
+
+        if (isset($validated['permissions'])) {
+            $role->givePermissionTo($validated['permissions']);
+        }
+
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $role = Role::findOrFail($id);
+        $permissions = Permission::all();
+        return view('roles.edit', compact('role', 'permissions'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:roles,name,' . $id,
+            'permissions' => 'array',
+        ]);
+
+        $role = Role::findOrFail($id);
+        $role->update(['name' => $validated['name']]);
+        $role->syncPermissions($validated['permissions'] ?? []);
+
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $role = Role::findOrFail($id);
+
+        if (in_array($role->name, ['admin', 'client'])) {
+            return redirect()->route('roles.index');
+        }
+
+        $role->delete();
+        return redirect()->route('roles.index');
+    }
+}
