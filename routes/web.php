e68081b548478c1e080238d3b2e958c9c7371bf3
youssef-boudouar <youssefboudouar771@gmail.com>
2026-02-18 17:18:58 +0000

    Add admin user and role management routes, and refactor product routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,43 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
-
+// public
 Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/search', [ProductController::class, 'search'])->name('search');
-Route::resource('products', ProductController::class);
 
+// register
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
+
+// login
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
+// logout
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
-
+// favorites
 Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
 Route::post('/favorites/{product}', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
+
+// Admin actions
+Route::get('/products/create', [ProductController::class, 'create'])->middleware(['auth'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->middleware(['auth'])->name('products.store');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->middleware(['auth'])->name('products.edit');
+Route::put('/products/{product}', [ProductController::class, 'update'])->middleware(['auth'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->middleware(['auth'])->name('products.destroy');
+
+
+
+Route::resource('admin/users', UserController::class)->middleware(['auth'])->names('admin.users');
+Route::resource('roles', RoleController::class)->middleware(['auth']);
